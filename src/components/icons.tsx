7c4fa19f4173d02e8981a8e5b0d7e803f8b7ac9b
Netbizowerri <netbiz0925@gmail.com>
2026-04-20 13:05:11 +0100

    fix: Remove duplicate LekeElekee key in Icons object

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -165,14 +165,5 @@ export const Icons = {
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" fill="none" stroke="currentColor" strokeWidth="2"/>
       </svg>
     ),
-    LekeElekee: () => (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        className="w-full h-full"
-      >
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" fill="none" stroke="currentColor" strokeWidth="2"/>
-      </svg>
-    ),
 };
     
